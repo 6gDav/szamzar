9cee03a35d285e26abff5e6a975ac31c86e1f752
6gDav <118929714+6gDav@users.noreply.github.com>
2023-06-08 09:54:59 +0200

    update

--- script.js
+++ script.js
@@ -1,89 +1,34 @@
-const display = document.getElementById('display')
-const numberButtons = document.querySelectorAll('.number')
-const operatorButtons = document.querySelectorAll('.operator')
-const clearButton = document.getElementById('clearBtn')
-const calculateButton = document.getElementById('calculateBtn')
+document.addEventListener('DOMContentLoaded', function() {
+  let code = '';
 
-let currentInput = '0'
-let previousInput = '0'
-let currentOperator = null
-let shouldClearDisplay = false
-
-numberButtons.forEach((button) => {
-  button.addEventListener('click', function () {
-    appendNumber(button.textContent)
-  })
-})
-
-operatorButtons.forEach((button) => {
-  button.addEventListener('click', function () {
-    setOperator(button.textContent)
-  })
-})
-
-clearButton.addEventListener('click', clearDisplay)
-calculateButton.addEventListener('click', calculate)
-
-document.addEventListener('keydown', function (event) {
-  const key = event.key
-  if (/[0-9]/.test(key)) {
-    appendNumber(key)
-  } else if (['+', '-', '*', '/'].includes(key)) {
-    setOperator(key)
-  } else if (key === 'Enter') {
-    calculate()
-  } else if (key === 'Backspace') {
-    clearDisplay()
+  function addNumber(number) {
+    code += number;
+    document.getElementById('code').value = code;
   }
-})
 
-function appendNumber(number) {
-  if (currentInput === '0' || shouldClearDisplay) {
-    currentInput = number
-    shouldClearDisplay = false
-  } else {
-    currentInput += number
+  function submitCode() {
+    const enteredCode = code;
+
+    if (enteredCode === '1234') {
+      displayMessage('Helyes kód!');
+    } else {
+      displayMessage('Hibás kód!');
+    }
+
+    code = '';
+    document.getElementById('code').value = '';
   }
-  updateDisplay()
-}
 
-function setOperator(operator) {
-  if (currentOperator !== null) {
-    calculate()
+  function displayMessage(message) {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = message;
   }
-  previousInput = currentInput
-  currentOperator = operator
-  shouldClearDisplay = true
-}
 
-function clearDisplay() {
-  currentInput = '0'
-  previousInput = '0'
-  currentOperator = null
-  shouldClearDisplay = false
-  updateDisplay()
-}
-
-function calculate() {
-  const previous = parseFloat(previousInput)
-  const current = parseFloat(currentInput)
-  let result = 0
-  if (currentOperator === '+') {
-    result = previous + current
-  } else if (currentOperator === '-') {
-    result = previous - current
-  } else if (currentOperator === '*') {
-    result = previous * current
-  } else if (currentOperator === '/') {
-    result = previous / current
+  const numberButtons = document.getElementsByClassName('button');
+  for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', function(event) {
+      const number = event.target.textContent;
+      addNumber(number);
+    });
   }
-  currentInput = result.toString()
-  previousInput = '0'
-  currentOperator = null
-  shouldClearDisplay = true
-  updateDisplay()
-}
-
-function updateDisplay() {
-  display.textContent = currentInput
-}
+});
